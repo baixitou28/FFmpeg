4d849b46fcbf3c635ff51cc6f873146b585d702b
tiger <baixitou28@hotmail.com>
2020-06-03 20:50:30 +0800

    new_video_stream

--- fftools/ffmpeg_opt.c
+++ fftools/ffmpeg_opt.c
@@ -1655,12 +1655,12 @@ static OutputStream *new_video_stream(OptionsContext *o, AVFormatContext *oc, in
     OutputStream *ost;
     AVCodecContext *video_enc;
     char *frame_rate = NULL, *frame_aspect_ratio = NULL;
-
+    //01. 创建
     ost = new_output_stream(o, oc, AVMEDIA_TYPE_VIDEO, source_index);
     st  = ost->st;
     video_enc = ost->enc_ctx;
-
-    MATCH_PER_STREAM_OPT(frame_rates, str, frame_rate, oc, st);
+    //02. 可选项
+    MATCH_PER_STREAM_OPT(frame_rates, str, frame_rate, oc, st);//02.01 frame_rates
     if (frame_rate && av_parse_video_rate(&ost->frame_rate, frame_rate) < 0) {
         av_log(NULL, AV_LOG_FATAL, "Invalid framerate value: %s\n", frame_rate);
         exit_program(1);
@@ -1668,7 +1668,7 @@ static OutputStream *new_video_stream(OptionsContext *o, AVFormatContext *oc, in
     if (frame_rate && video_sync_method == VSYNC_PASSTHROUGH)
         av_log(NULL, AV_LOG_ERROR, "Using -vsync 0 and -r can produce invalid output files\n");
 
-    MATCH_PER_STREAM_OPT(frame_aspect_ratios, str, frame_aspect_ratio, oc, st);
+    MATCH_PER_STREAM_OPT(frame_aspect_ratios, str, frame_aspect_ratio, oc, st);//02.02 frame_aspect_ratios
     if (frame_aspect_ratio) {
         AVRational q;
         if (av_parse_ratio(&q, frame_aspect_ratio, 255, 0, NULL) < 0 ||
@@ -1679,11 +1679,11 @@ static OutputStream *new_video_stream(OptionsContext *o, AVFormatContext *oc, in
         ost->frame_aspect_ratio = q;
     }
 
-    MATCH_PER_STREAM_OPT(filter_scripts, str, ost->filters_script, oc, st);
+    MATCH_PER_STREAM_OPT(filter_scripts, str, ost->filters_script, oc, st);//02.03 filters
     MATCH_PER_STREAM_OPT(filters,        str, ost->filters,        oc, st);
     if (o->nb_filters > 1)
         av_log(NULL, AV_LOG_ERROR, "Only '-vf %s' read, ignoring remaining -vf options: Use ',' to separate filters\n", ost->filters);
-
+    //03. 非copy模式
     if (!ost->stream_copy) {
         const char *p = NULL;
         char *frame_size = NULL;
@@ -1692,26 +1692,26 @@ static OutputStream *new_video_stream(OptionsContext *o, AVFormatContext *oc, in
         char *chroma_intra_matrix = NULL;
         int do_pass = 0;
         int i;
-
+        //03.01. frame_sizes
         MATCH_PER_STREAM_OPT(frame_sizes, str, frame_size, oc, st);
         if (frame_size && av_parse_video_size(&video_enc->width, &video_enc->height, frame_size) < 0) {
             av_log(NULL, AV_LOG_FATAL, "Invalid frame size: %s.\n", frame_size);
             exit_program(1);
         }
-
+        //03.02.
         video_enc->bits_per_raw_sample = frame_bits_per_raw_sample;
         MATCH_PER_STREAM_OPT(frame_pix_fmts, str, frame_pix_fmt, oc, st);
         if (frame_pix_fmt && *frame_pix_fmt == '+') {
             ost->keep_pix_fmt = 1;
             if (!*++frame_pix_fmt)
                 frame_pix_fmt = NULL;
-        }
+        }//03.03.
         if (frame_pix_fmt && (video_enc->pix_fmt = av_get_pix_fmt(frame_pix_fmt)) == AV_PIX_FMT_NONE) {
             av_log(NULL, AV_LOG_FATAL, "Unknown pixel format requested: %s.\n", frame_pix_fmt);
             exit_program(1);
-        }
+        }//03.04.
         st->sample_aspect_ratio = video_enc->sample_aspect_ratio;
-
+        //03.05. intra
         if (intra_only)
             video_enc->gop_size = 0;
         MATCH_PER_STREAM_OPT(intra_matrices, str, intra_matrix, oc, st);
@@ -1740,7 +1740,7 @@ static OutputStream *new_video_stream(OptionsContext *o, AVFormatContext *oc, in
             }
             parse_matrix_coeffs(video_enc->inter_matrix, inter_matrix);
         }
-
+        //03.06.
         MATCH_PER_STREAM_OPT(rc_overrides, str, p, oc, st);
         for (i = 0; p; i++) {
             int start, end, q;
@@ -1770,10 +1770,10 @@ static OutputStream *new_video_stream(OptionsContext *o, AVFormatContext *oc, in
             if (p) p++;
         }
         video_enc->rc_override_count = i;
-
+        //03.07.
         if (do_psnr)
             video_enc->flags|= AV_CODEC_FLAG_PSNR;
-
+        //03.08.
         /* two pass mode */
         MATCH_PER_STREAM_OPT(pass, i, do_pass, oc, st);
         if (do_pass) {
@@ -1786,12 +1786,12 @@ static OutputStream *new_video_stream(OptionsContext *o, AVFormatContext *oc, in
                 av_dict_set(&ost->encoder_opts, "flags", "+pass2", AV_DICT_APPEND);
             }
         }
-
+        //03.09.
         MATCH_PER_STREAM_OPT(passlogfiles, str, ost->logfile_prefix, oc, st);
         if (ost->logfile_prefix &&
             !(ost->logfile_prefix = av_strdup(ost->logfile_prefix)))
             exit_program(1);
-
+        //03.10.
         if (do_pass) {
             char logfilename[1024];
             FILE *f;
@@ -1825,24 +1825,24 @@ static OutputStream *new_video_stream(OptionsContext *o, AVFormatContext *oc, in
                 }
             }
         }
-
+        //03.11.
         MATCH_PER_STREAM_OPT(forced_key_frames, str, ost->forced_keyframes, oc, st);
         if (ost->forced_keyframes)
             ost->forced_keyframes = av_strdup(ost->forced_keyframes);
-
+        //03.12.
         MATCH_PER_STREAM_OPT(force_fps, i, ost->force_fps, oc, st);
-
+        //03.13.
         ost->top_field_first = -1;
         MATCH_PER_STREAM_OPT(top_field_first, i, ost->top_field_first, oc, st);
 
-
+        //03.14.
         ost->avfilter = get_ost_filters(o, oc, ost);
         if (!ost->avfilter)
             exit_program(1);
-    } else {
-        MATCH_PER_STREAM_OPT(copy_initial_nonkeyframes, i, ost->copy_initial_nonkeyframes, oc ,st);
+    } else {//03.15.
+        MATCH_PER_STREAM_OPT(copy_initial_nonkeyframes, i, ost->copy_initial_nonkeyframes, oc ,st);//如果是copy模式，是否要复制一开始不是i帧？
     }
-
+    //04. copy模式
     if (ost->stream_copy)
         check_streamcopy_filters(o, oc, ost, AVMEDIA_TYPE_VIDEO);
 
